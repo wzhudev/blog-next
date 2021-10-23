30454058a0353958ad942eb11625a8637c26ac99
huwenzhao <huwenzhao@bytedance.com>
2021-10-23 22:20:35 +0800

    :bug: fix feed

--- scripts/gen-rss.js
+++ scripts/gen-rss.js
@@ -5,9 +5,9 @@ const matter = require('gray-matter')
 
 async function generate() {
   const feed = new RSS({
-    title: 'Your Name',
-    site_url: 'https://yoursite.com',
-    feed_url: 'https://yoursite.com/feed.xml'
+    title: 'Wendell',
+    site_url: 'https://wendell.fun',
+    feed_url: 'https://wendell.fun/feed.xml'
   })
 
   const posts = await fs.readdir(path.join(__dirname, '..', 'pages', 'posts'))
